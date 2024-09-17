9f69181dbdcc08a870943affe43212e5a84cfad4
Yash Virsodiya <virsodiyayash4@gmail.com>
2024-09-17 18:52:51 +0530

    React made by custom

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,14 @@
 import Chai from "./Chai";
 
 function App() {
+
+  const username = 'yash patel'
+
   return (
+  <>
    <Chai />
+   <h1>Hello chai aur code from {username}</h1>
+   </>
   );
 }
 
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// const reactElement = {
+//   type : 'a',
+//   props : {
+//       href : 'https://google.com',
+//       target : '_blank'
+//   },
+//   children : 'click me to visit google'
+// }
+
+// const anotherElement = (
+//   <a href='https://google.com' target='_blank'>visit google</a>
+// )
+
+const reactElement = React.createElement(
+  'a',
+  {href : 'https://google.com' , target : '_blank'},
+  'click me to visit google'
+)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  /*
+   reactElement
+   */
+  <App/>
 );
 
