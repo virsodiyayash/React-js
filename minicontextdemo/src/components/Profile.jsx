0b1e3e6f65f767764e5b0084706d569df86297f7
Yash Virsodiya <virsodiyayash4@gmail.com>
2025-09-11 00:59:13 +0530

    Learn about context

---
+++ minicontextdemo/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react"
+import React  from 'react'
+import UserContext from "../context/usercontext"
+
+function Profile() {
+    const {user} = useContext(UserContext)
+    
+    if(!user) return <div>Please login</div>
+
+    return <div>Welcome {user.username}</div>
+}
+
+export default Profile
--- todocontextlocal/src/context/TodoContext.js
+++ todocontextlocal/src/context/TodoContext.js
@@ -1,11 +1,17 @@
 import { createContext , useContext } from "react";
 
 export const TodoContext = createContext({
-
+    todos:[
+        {
+            id: 1,
+            todo : "Todo msg",
+            completed : false
+        }
+    ]
 })
 
 export const useTodo = () => {
-    return useContext(TodoContextx)
+    return useContext(TodoContext)
 }
 
 export const TodoProvider = TodoContext.Provider
